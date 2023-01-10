0916d83de3a6358208c6880253cf9691f2df90e7
Anton Danilov <adanilovx@gmail.com>
2023-01-10 21:23:55 +0400

    Fifth decision commit

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task2;
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int age;;
+    private int age;
     private int phoneNumber;
 
     void callToFriend() {
